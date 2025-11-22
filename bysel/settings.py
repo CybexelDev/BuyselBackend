2dfa0a939cc4270053b87236114c2e6144693b70
doodles <nmedevelopercommunity@gmail.com>
2025-11-22 14:29:41 +0530

    share

--- bysel/settings.py
+++ bysel/settings.py
@@ -88,7 +88,7 @@ ROOT_URLCONF = 'bysel.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

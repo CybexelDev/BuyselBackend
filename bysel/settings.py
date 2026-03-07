dd0d2bc47b838f2e12fe4ae82346a1446bfee38c
doodles <nmedevelopercommunity@gmail.com>
2026-03-07 13:03:07 +0530

    cors allowed render

--- bysel/settings.py
+++ bysel/settings.py
@@ -72,6 +72,8 @@ CSRF_TRUSTED_ORIGINS = [
 CORS_ALLOWED_ORIGINS = [
    "http://localhost:5173"
 ]
+CORS_ALLOW_CREDENTIALS = True
+
 
 # CSRF_TRUSTED_ORIGINS = [
 #     "https://cybexeltechnologies.com",
@@ -86,10 +88,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+
+
+
     
 ] 
 

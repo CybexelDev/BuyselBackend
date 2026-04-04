086beeb28209bd5f1924434d11c9ff05ef5dfcb7
N-meapp <developersnme@gmail.com>
2026-04-04 10:11:33 +0530

    updated

--- bysel/settings.py
+++ bysel/settings.py
@@ -139,10 +139,11 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 #     }
 # }
 
+# settings.py
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Path to your SQLite file
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # make sure BASE_DIR/db.sqlite3 exists
     }
 }
 
